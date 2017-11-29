adfb218c835e5ca890d59ddd8747eebbca348a8c
Scott Kuchinski <scottkuchinski@gmail.com>
2017-11-28 19:52:21 -0500

    front end built, JSON parse error still present

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Container,
+  Header,
+  Segment,
+  Button,
+  Icon,
+  Dimmer,
+  Loader,
+  Divider
+} from 'semantic-ui-react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+    this.getDrinks = this.getDrinks.bind(this);
+    this.getDrink = this.getDrink.bind(this);
+  }
   componentDidMount() {
-    window
-      .fetch('api/drinks')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .catch(error => console.log(error));
+    this.getDrinks();
+  }
+  fetch(endpoint) {
+    return new Promise((resolve, reject) => {
+      window
+        .fetch(endpoint)
+        .then(response => response.json())
+        .then(json => resolve(json))
+        .catch(error => reject(error));
+    });
+  }
+  getDrinks() {
+    this.fetch('api/drinks').then(drinks => {
+      this.setState({ drinks: drinks });
+      this.getDrink(drinks[0].id);
+    });
+  }
+  getDrink(id) {
+    this.fetch(`api/drinks/${id}`).then(drink =>
+      this.setState({ drink: drink })
+    );
   }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+    let { drinks, drink } = this.state;
+    return drinks ? (
+      <Container text>
+        <Header as="h2" icon textAlign="center">
+          <Icon name="cocktail" circular />
+          <Header.Content>List of Ingredients</Header.Content>
+        </Header>
+        <Button.Group fluid widths={drinks.length}>
+          {Object.keys(drinks).map(key => {
+            return (
+              <Button
+                active={drink && drink.id === drinks[key].id}
+                fluid
+                key={key}
+                onClick={() => this.getDrink(drinks[key].id)}
+              >
+                {drinks[key].title}
+              </Button>
+            );
+          })}
+        </Button.Group>
+        <Divider hidden />
+        {drink && (
+          <Container>
+            <Header as="h2">{drink.title}</Header>
+            {drink.description && <p>{drink.description}</p>}
+            {drink.ingredients && (
+              <Segment.Group>
+                {drink.ingredients.map((ingredient, i) => (
+                  <Segment key={i}>{ingredient.description}</Segment>
+                ))}
+              </Segment.Group>
+            )}
+            {drink.steps && <p>{drink.steps}</p>}
+          </Container>
+        )}
+      </Container>
+    ) : (
+      <Container text>
+        <Dimmer active inverted>
+          <Loader content="Loading" />
+        </Dimmer>
+      </Container>
     );
   }
 }
